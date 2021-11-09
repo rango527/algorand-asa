f61e9e9a7c9f6f0a827f2211a12a05865f934753
rango527 <web.mobile497373@gmail.com>
2021-11-10 00:44:06 +0800

    generate account

---
+++ script/generate_account.js
@@ -0,0 +1,7 @@
+const algosdk = require('algosdk');
+
+let account = algosdk.generateAccount();
+console.log("Account Address: ", account.addr);
+
+let mn = algosdk.secretKeyToMnemonic(account.sk);
+console.log("Account Mnemonic: ", mn);
